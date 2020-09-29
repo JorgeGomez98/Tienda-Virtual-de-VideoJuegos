9d549ec56158a65272ad0dab2abdbd61ba20afbf
JorgeGomez98 <48143414+JorgeGomez98@users.noreply.github.com>
2020-09-29 10:21:24 -0500

    prueba

---
+++ FJ2/App_Code/Prueba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Prueba
+/// </summary>
+public class Prueba
+{
+    public Prueba()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
