e5287915d51b0adb231729d5afd3bcf63d8aae5d
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2020-09-30 20:31:59 -0500

    Login
    Implementación de clases administrador y modulador al login
    Posee errores

--- FJ2/Controller/Login.aspx.cs
+++ FJ2/Controller/Login.aspx.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class View_Login : System.Web.UI.Page
@@ -14,7 +10,35 @@ public partial class View_Login : System.Web.UI.Page
 
     protected void LN_principal_Authenticate(object sender, AuthenticateEventArgs e)
     {
-        Usuario usuario = new Usuario();
+        Moderador moderador = new Moderador();
+        moderador.Nickname = LN_principal.UserName;
+        moderador.Contraseña = LN_principal.Password;
+
+        moderador = new DAOUsuario().loginModerador(moderador);
+        if (moderador == null)
+        {
+            ((Label)LN_principal.FindControl("L_Mensaje")).Text = "Usuario o clave incorrecto";
+        }
+        else
+        {
+            Response.Redirect("Crude.aspx");
+        }
+        /*Administrador administrador = new Administrador();
+        administrador.Nickname = LN_principal.UserName;
+        administrador.Contraseña = LN_principal.Password;
+
+        administrador = new DAOUsuario().loginAdmin(administrador);
+        if (administrador == null)
+        {
+            ((Label)LN_principal.FindControl("L_Mensaje")).Text = "Usuario o clave incorrecto";
+        }
+        else
+        {
+            Response.Redirect("Crude.aspx");
+        }*/
+
+
+        /*Usuario usuario = new Usuario();
         usuario.Nickname = LN_principal.UserName;
         usuario.Contraseña = LN_principal.Password;
 
@@ -26,6 +50,52 @@ public partial class View_Login : System.Web.UI.Page
         else
         {
             Response.Redirect("Crude.aspx");
+        }*/
+
+        /*Administrador administrador = new Administrador();
+        administrador.Nickname = LN_principal.UserName;
+        administrador.Contraseña = LN_principal.Password;        
+
+        administrador = new DAOUsuario().loginAdmin(administrador);
+        
+        if(administrador == null)
+        {
+            Moderador moderador = new Moderador();
+
+            moderador.Nickname = LN_principal.UserName;
+            moderador.Contraseña = LN_principal.Password;
+
+            moderador = new DAOUsuario().loginModerador(moderador);
+
+            if (moderador== null)
+            {
+                Usuario usuario = new Usuario();
+
+                usuario.Nickname = LN_principal.UserName;
+                usuario.Contraseña = LN_principal.Password;
+
+                usuario = new DAOUsuario().login(usuario);
+                if(usuario == null)
+                {
+                    ((Label)LN_principal.FindControl("L_Mensaje")).Text = "Usuario o clave incorrecto";
+                }
+                else
+                {
+                    Response.Redirect("CrudeUsuario.aspx");
+                }
+                
+            }
+            else
+            {
+                Response.Redirect("CrudeModerador.aspx");
+            }
         }
+        else
+        {
+            Response.Redirect("Crude.aspx");
+        }
+
+
+        */
     }
 }
