60df4f3f33094921bc5d699de829d88796946003
anfeacosta7 <55371890+anfeacosta7@users.noreply.github.com>
2020-10-01 20:01:42 -0500

    prueba alan

---
+++ FJ2/App_Code/Encapsular/prueba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de prueba
+/// </summary>
+public class prueba
+{
+    private int prueb;
+}
