9026d5b72b9ea0854de4f421e574d1f41fe8c1aa
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2020-10-20 08:28:27 -0500

    Arreglo diseño en Registro

--- FJ2/Controller/Login.aspx.cs
+++ FJ2/Controller/Login.aspx.cs
@@ -8,25 +8,6 @@ public partial class View_Login : System.Web.UI.Page
 
     }
 
-    /*protected void LN_principal_Authenticate(object sender, AuthenticateEventArgs e)
-    {
-        Usuario usuario = new Usuario();
-        usuario.Nickname = LN_principal.UserName;
-        usuario.Contraseña = LN_principal.Password;
-
-        usuario = new DAOUsuario().login(usuario);
-        if (usuario == null)
-        {
-            ((Label)LN_principal.FindControl("L_Mensaje")).Text = "Usuario o clave incorrecto";
-            ///Session["user"] = null;
-        }
-        else
-        {
-            Response.Redirect("Crude.aspx");
-        }
-        
-    }*/
-
     protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
     {
         Usuario usuario = new Usuario();
