695f2ce5008bde7387d6f5808a39f443b2ab7647
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2020-10-27 08:26:09 -0500

    arreglo

---
+++ FJ2/App_Code/Datos/DAOBiblioteca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de DAOBiblioteca
+/// </summary>
+public class DAOBiblioteca
+{
+
+
+}
