d3b5a968923c4612b0c34f01bf7e2f0f1995ee4b
JorgeGomez98 <48143414+JorgeGomez98@users.noreply.github.com>
2020-11-07 07:28:31 -0500

    Update RegistroUsuario.aspx.cs

--- FJ2/Controller/RegistroUsuario.aspx.cs
+++ FJ2/Controller/RegistroUsuario.aspx.cs
@@ -10,49 +10,7 @@ using System.Web.UI.WebControls;
 
 public partial class View_RegistroUsuario : System.Web.UI.Page
 {
-    /*public bool IsValidEmail(string correo)
-    {
-        if (string.IsNullOrWhiteSpace(correo))
-            return false;
 
-        try
-        {
-            // Normalize the domain
-            correo = Regex.Replace(correo, @"(@)(.+)$", DomainMapper,
-                                  RegexOptions.None, TimeSpan.FromMilliseconds(200));
-
-            // Examines the domain part of the email and normalizes it.
-            string DomainMapper(Match match)
-            {
-                // Use IdnMapping class to convert Unicode domain names.
-                var idn = new IdnMapping();
-
-                // Pull out and process domain name (throws ArgumentException on invalid)
-                string domainName = idn.GetAscii(match.Groups[2].Value);
-
-                return match.Groups[1].Value + domainName;
-            }
-        }
-        catch (RegexMatchTimeoutException e)
-        {
-            return false;
-        }
-        catch (ArgumentException e)
-        {
-            return false;
-        }
-
-        try
-        {
-            return Regex.IsMatch(correo,
-                @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
-                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-        }
-        catch (RegexMatchTimeoutException)
-        {
-            return false;
-        }
-    }*/
 
 
 
